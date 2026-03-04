ed806faaa48c4b0aaced76a75d5de7e219a0fee0
Rodrigo-pixel44 <fleitasrodrigo31@gmail.com>
2026-03-03 22:28:06 -0300

    Modificação Readme.md

--- Program.cs
+++ Program.cs
@@ -8,10 +8,10 @@ namespace aula03RH
         static void Main(string[] args)
         {
             Funcionario func = new Funcionario();
-            /*
+            
             func.Id = 10;
             func.Nome = "Neymar";
-            func.Cpf = "12345678910"; */
+            func.Cpf = "12345678910"; 
             func.DataAdmissao = DateTime.Parse("01/01/2010");
             func.Salario = 1000.00M;
             func.Tipofuncionario = Models.Enuns.TipofuncionarioEnum.CLT;
