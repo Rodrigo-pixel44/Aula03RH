0b6bc54afde376b1f6ed17fc2bc40b58f8cac836
Rodrigo-pixel44 <fleitasrodrigo31@gmail.com>
2026-03-03 22:27:47 -0300

    Modificação Readme.md

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using aula03RH.Models;
+
+namespace aula03RH
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+            /*
+            func.Id = 10;
+            func.Nome = "Neymar";
+            func.Cpf = "12345678910"; */
+            func.DataAdmissao = DateTime.Parse("01/01/2010");
+            func.Salario = 1000.00M;
+            func.Tipofuncionario = Models.Enuns.TipofuncionarioEnum.CLT;
+
+            string mensagem = func.ExibirPeriodoExperiencia();
+            Console.WriteLine(mensagem);
+        }
+    }
+}
