98bdc8b857951c126ba06d9ef9100213168158fa
Rodrigo-pixel44 <fleitasrodrigo31@gmail.com>
2026-03-03 21:40:59 -0300

    Aula 03 - Classes e Enuns

---
+++ Models/Funcionario.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aula03RH.Models
+{
+    public class Funcionario
+    {
+        
+    }
+}
