144b346114d67b656d3d546693ac3f5300ffe5cc
rodrigo-pixel44 <fleitasrodrigo31@gmail.com>
2026-08-26 21:53:52 -0300

    ....

--- obj/Debug/net10.0/Aula03RH.AssemblyInfo.cs
+++ obj/Debug/net10.0/Aula03RH.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula03RH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c8ded996b46156041e7bf6033da0c3cc305af1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b964808cb2591cb0808903723cd6c04f7c8c739")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula03RH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula03RH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
